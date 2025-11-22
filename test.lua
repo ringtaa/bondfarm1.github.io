5d141ba7f6a2629b895686cb7363889284bcc249
ringtaa <eeeiqjjj50@gmail.com>
2025-11-22 15:16:08 -0500

    Change button title from 'New Custom Guard Powers' to 'Glass Vision Fixed'

--- test.lua
+++ test.lua
@@ -66,7 +66,7 @@ Tabs.Jump:Button({
 })
 
 Tabs.Jump:Button({
-    Title = "New Custom Guard Powers",
+    Title = "Glass Vision Fixed",
     Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ringtaa/castletpfast.github.io/refs/heads/main/FASTCASTLE.lua"))()
     end,
